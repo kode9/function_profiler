f911c612eee52950b58008a3c22d8cbb46b2f5e5
Pierre-Luc Perrier <pluc@the-pluc.net>
2015-05-25 18:18:49 +0200

    Finish implementing the first version
    - Use boost thread clock
    - Use boost accumulators
    - Add FUNCTION_PROFILER macro
    
    The thread clock is nice but not necessarly representative, should
    propably add a steady clock as well.

--- function_profiler.h
+++ function_profiler.h
@@ -1,49 +1,103 @@
 #ifndef function_profiler_h
 #define function_profiler_h
 
-#include <thread>
-#include <glog/logging.h>
-#include <glog/raw_logging.h>
+// http://preshing.com/20111203/a-c-profiling-module-for-multithreaded-apis/
 
 #include <boost/chrono.hpp>
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics/min.hpp>
+#include <boost/accumulators/statistics/max.hpp>
+#include <boost/accumulators/statistics/mean.hpp>
+#include <boost/accumulators/statistics/sum.hpp>
+
+#include <string> // std::string
+#include <thread> // std::this_thread::get_id
+#include <cstdio> // std::printf
+#include <ios>    // std::hex
 
 struct function_profiler_stats
 {
-  using clock = boost::chrono::thread_clock;
-  using ms = boost::chrono::duration<double, boost::milli>;
+  // Clock
+  using clock           = boost::chrono::thread_clock;
+  using duration        = clock::duration;
+  using time_point      = clock::time_point;
+  using double_duration = boost::chrono::duration<double, duration::period>;
+  using ms              = boost::chrono::duration<double, boost::milli>;
 
-  int m_count;
-  clock::duration m_total;
+  // Accumulator
+  using min_tag  = boost::accumulators::tag::min;
+  using max_tag  = boost::accumulators::tag::max;
+  using mean_tag = boost::accumulators::tag::mean;
+  using sum_tag  = boost::accumulators::tag::sum;
+  using features = boost::accumulators::features<min_tag, max_tag, mean_tag, sum_tag>;
 
-  function_profiler_stats() : m_count{0}, m_total{0} {}
+  function_profiler_stats() = delete;
+  function_profiler_stats(const std::string &name)
+  {
+    std::ostringstream os;
+    os << "[function_profiler][" << std::hex << std::this_thread::get_id() << "][" << name << "]";
+    m_prefix = os.str();
+    m_last = m_last_report = clock::now();
+  }
 
-  inline void increment() { ++m_count; }
-  inline void add_time(const clock::duration &duration) { m_total += duration; }
+  ~function_profiler_stats()
+  {
+    report();
+  }
 
-  inline void print() const {
-    if (m_count % 10 == 0) {
-      auto tid = std::this_thread::get_id();
-      std::ostringstream os; os << tid;
-      RAW_LOG(INFO, "Thread %s: %d | %F", os.str().c_str(), m_count, ms{m_total}.count());
+  inline void start() {
+    m_last = clock::now();
+  }
+
+  inline void stop() {
+    const auto now = clock::now();
+    m_accumulator(duration{now - m_last}.count());
+    m_last = std::move(now);
+
+    // Check if we need to report current stats
+    if (m_last - m_last_report > report_interval) {
+      m_last_report = m_last;
+      report();
     }
   }
+
+private:
+  inline void report() const {
+    std::printf("%s #%02lu, min %.5f, max %.5f, mean %.5f, total %.5f\n",
+                m_prefix.c_str(),
+                boost::accumulators::count(m_accumulator),
+                ms{duration{boost::accumulators::min(m_accumulator)}}.count(),
+                ms{duration{boost::accumulators::max(m_accumulator)}}.count(),
+                ms{double_duration{boost::accumulators::mean(m_accumulator)}}.count(),
+                ms{duration{boost::accumulators::sum(m_accumulator)}}.count());
+  }
+
+private:
+  static constexpr auto report_interval = boost::chrono::seconds{1};
+
+  ///< Statistics accumulator
+  boost::accumulators::accumulator_set<duration::rep, features> m_accumulator;
+  ///< Last update
+  time_point m_last;
+  ///< Last report
+  time_point m_last_report;
+  ///< Prefix used when reporting
+  std::string m_prefix;
 };
 
 struct function_profiler
 {
 public:
-  using clock = function_profiler_stats::clock;
 
   function_profiler(function_profiler_stats &stats)
-    : m_stats{stats}, m_start{clock::now()}
+    : m_stats{stats}
   {
-    m_stats.increment();
+    m_stats.start();
   }
 
   ~function_profiler()
   {
-    m_stats.add_time(clock::now() - m_start);
-    m_stats.print();
+    m_stats.stop();
   }
 
   function_profiler() = delete;
@@ -55,7 +109,12 @@ public:
 
 private:
   function_profiler_stats &m_stats;
-  clock::time_point m_start;
 };
+
+// Note: __func__ is not a preprocessor macro, it's a 'function-local
+// predefined variable' (char *), hence we don't expand it.
+#define PROFILE_FUNCTION()                              \
+  thread_local function_profiler_stats info{__func__};  \
+  function_profiler scoped_profiler{info};
 
 #endif // function_profiler_h
