f937fa7d48e42ca1e7983d3b8363969692d9a6b2
Pierre-Luc Perrier <pluc@the-pluc.net>
2015-07-11 01:16:16 +0200

    Add .clang-format, indent function_profiler.hpp

--- function_profiler.hpp
+++ function_profiler.hpp
@@ -2,11 +2,11 @@
 #define function_profiler_h
 
 /// Intrusive thread-safe function profiling
-/// @author Pierre-Luc Perrier <pluc@the-pluc.net>
-/// @version 0.2
 ///
-/// Based on an idea by Jeff Preshing:
-/// http://preshing.com/20111203/a-c-profiling-module-for-multithreaded-apis/
+/// Author:  Pierre-Luc Perrier <pluc@the-pluc.net>
+/// Version: 0.2
+/// URL:     https://git.the-pluc.net/function_profiler.git/
+///
 
 #ifdef FP_ENABLE
 
@@ -17,18 +17,15 @@
 #include <cstdio> // std::printf
 #include <ios>    // std::hex
 
-struct function_profiler_stats
-{
+struct function_profiler_stats {
   // Clock
-  using clock           = boost::chrono::thread_clock;
-  using duration        = clock::duration;
-  using time_point      = clock::time_point;
-  using ms              = boost::chrono::duration<double, boost::milli>;
+  using clock = boost::chrono::thread_clock;
+  using duration = clock::duration;
+  using time_point = clock::time_point;
+  using ms = boost::chrono::duration<double, boost::milli>;
 
   function_profiler_stats() = delete;
-  function_profiler_stats(const std::string &name)
-    : m_accumulator{0}
-    , m_count{0}
+  function_profiler_stats(const std::string &name) : m_accumulator{0}, m_count{0}
   {
     std::ostringstream os;
     os << "[FP][" << std::hex << std::this_thread::get_id() << "][" << name << "]";
@@ -41,11 +38,13 @@ struct function_profiler_stats
     report();
   }
 
-  inline void start() {
+  inline void start() noexcept
+  {
     m_last = clock::now();
   }
 
-  inline void stop() {
+  inline void stop()
+  {
     ++m_count;
     const auto now = clock::now();
     m_accumulator += now - m_last;
@@ -59,34 +58,26 @@ struct function_profiler_stats
   }
 
 private:
-  inline void report() const {
+  inline void report() const
+  {
     if (m_count == 0) return;
     auto ms_duration = ms{m_accumulator}.count();
-    std::printf("%s #%02lu, avg %.5Fms, tot %.5Fms\n",
-		m_prefix.c_str(), m_count, ms_duration / m_count, ms_duration);
+    std::printf("%s #%02lu, avg %.5Fms, tot %.5Fms\n", m_prefix.c_str(), m_count, ms_duration / m_count, ms_duration);
   }
 
 private:
   static constexpr auto report_interval = boost::chrono::seconds{1};
 
-  ///< Duration accumulator
-  duration m_accumulator;
-  ///< Number of samples
-  uint_least64_t m_count;
-  ///< Last update
-  time_point m_last;
-  ///< Last report
-  time_point m_last_report;
-  ///< Prefix used when reporting
-  std::string m_prefix;
+  duration m_accumulator;   // Duration accumulator
+  uint_least64_t m_count;   // Number of samples
+  time_point m_last;        // Last update
+  time_point m_last_report; // Last report
+  std::string m_prefix;     // Prefix used when reporting
 };
 
-struct function_profiler
-{
+struct function_profiler {
 public:
-
-  function_profiler(function_profiler_stats &stats)
-    : m_stats{stats}
+  function_profiler(function_profiler_stats &stats) : m_stats{stats}
   {
     m_stats.start();
   }
@@ -100,8 +91,8 @@ public:
   function_profiler(const function_profiler &) = delete;
   function_profiler(function_profiler &&) = delete;
 
-  function_profiler & operator =(const function_profiler &) = delete;
-  function_profiler & operator =(function_profiler &&) = delete;
+  function_profiler &operator=(const function_profiler &) = delete;
+  function_profiler &operator=(function_profiler &&) = delete;
 
 private:
   function_profiler_stats &m_stats;
@@ -111,8 +102,8 @@ constexpr boost::chrono::seconds function_profiler_stats::report_interval;
 
 // Note: __func__ is not a preprocessor macro, it's a 'function-local
 // predefined variable' (char *), hence we don't expand it.
-#define PROFILE_FUNCTION()                              \
-  thread_local function_profiler_stats info{__func__};  \
+#define PROFILE_FUNCTION()                                                                                             \
+  thread_local function_profiler_stats info{__func__};                                                                 \
   function_profiler scoped_profiler{info};
 
 #else // !FB_ENABLE
